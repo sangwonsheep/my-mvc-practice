2fd2ad6a15b77be54500232caee104430c8919b7
sangwonsheep <sangwonsheep@gmail.com>
2024-09-08 23:33:49 +0900

    서블릿 구현

---
+++ servlet-practice/src/main/java/org/example/servlet/CalculatorGenericServlet.java
@@ -0,0 +1,32 @@
+package org.example.servlet;
+
+import org.example.calculator.domain.Calculator;
+import org.example.calculator.domain.PositiveNumber;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+//@WebServlet("/calculate")
+public class CalculatorGenericServlet extends GenericServlet {
+
+    private static final Logger log = LoggerFactory.getLogger(CalculatorServlet.class);
+
+    @Override
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+        log.info("service");
+        int operand1 = Integer.parseInt(request.getParameter("operand1"));
+        String operator = request.getParameter("operator");
+        int operand2 = Integer.parseInt(request.getParameter("operand2"));
+
+        int result = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
+
+        PrintWriter writer = response.getWriter();
+        writer.println(result);
+    }
+}
